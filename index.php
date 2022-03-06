6366460e23b9ac5313a22572bf34e9e96a185b83
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-03-06 23:38:57 +0300

    Deuplicated

--- index.php
+++ index.php
@@ -1,34 +1,33 @@
-<html>
-    <body>
-        <!--name.php to be called on form submission-->
-        <form method = 'post'>
-            <h4>SELECT SUBJECTS</h4>
-             
-            <select name = 'subject[]' multiple size = 6> 
-                <option value = 'english'>ENGLISH</option>
-                <option value = 'maths'>MATHS</option>
-                <option value = 'computer'>COMPUTER</option>
-                <option value = 'physics'>PHYSICS</option>
-                <option value = 'chemistry'>CHEMISTRY</option>
-                <option value = 'hindi'>HINDI</option>
-            </select>
-            <input type = 'submit' name = 'submit' value = Submit>
-        </form>
-    </body>
-</html>
-<?php
-     
-    // Check if form is submitted successfully
-    if(isset($_POST["submit"]))
-    {
-        // Check if any option is selected
-        if(isset($_POST["subject"]))
-        {
-            // Retrieving each selected option
-            foreach ($_POST['subject'] as $subject)
-                print "You selected $subject<br/>";
+<?php 
+    $arr = array(1,5,2,5,1,3,2,4,5);
+    echo "Original Array : \n";
+    print_r($arr);
+    $arr2 = array_flip($arr);
+    echo '</br>';
+    $arr2 = array_flip($arr2);
+  
+    // re-order the array keys
+    $arr2= array_values($arr2);
+  
+    // print updated array
+    echo "\nUpdated Array : \n ";
+    print_r($arr2);
+
+    $uniqueArry = array();
+ 
+foreach($arr as $val) { //Loop1 
+    
+    foreach($uniqueArry as $uniqueValue) { //Loop2 
+
+        if($val == $uniqueValue) {
+            continue 2; // Referring Outer loop (Loop 1)
         }
-    else
-        echo "Select an option first !!";
     }
+    $uniqueArry[] = $val;
+}
+echo "</br>";
+echo "\nOther Slotion : \n ";
+    print_r($arr);
+    echo "</br>";
+print_r($uniqueArry);
 ?>
